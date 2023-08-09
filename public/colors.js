741875cf2114337a180355b6c61cf91ca4414729
Joi Misenti <joipolloi1@gmail.com>
2023-08-09 13:21:32 -0400

    added dynamic dots background to colors page

--- public/colors.js
+++ public/colors.js
@@ -143,6 +143,49 @@ async function handleDelete(event) {
         }
     }
 }
+
+// Animated Background for the balls bouncing around page
+// Some random colors // default: ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#885A5A"]
+const ballColors = ["#f6bd60", "#f7ede2", "#f5cac3", "#84a59d", "#f28482"];
+
+const numBalls = 50;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+    let ball = document.createElement("div");
+    ball.classList.add("ball");
+    ball.style.background = ballColors[Math.floor(Math.random() * ballColors.length)];
+    ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+    ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+    ball.style.transform = `scale(${Math.random()})`;
+    ball.style.width = `${Math.random()}em`;
+    ball.style.height = ball.style.width;
+
+    balls.push(ball);
+    document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+    let to = {
+        x: Math.random() * (i % 2 === 0 ? -11 : 11),
+        y: Math.random() * 12
+};
+
+let anim = el.animate(
+    [
+        { transform: "translate(0, 0)" },
+        { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+        {
+        duration: (Math.random() + 1) * 2000, // random duration
+        direction: "alternate",
+        fill: "both",
+        iterations: Infinity,
+        easing: "ease-in-out"
+        }
+    );
+});
   
 // Call the function to display color labels on page load
 document.addEventListener('DOMContentLoaded', async () => {
